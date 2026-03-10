c9cb401851e3ab07c70811fcc4498be1b6a7822e
bowo rahayu <rahayuributwibowo@gmail.com>
2026-03-11 04:00:16 +0700

    refactor(Article): Remove automatic url_hash generation
    Removes the `saving` model event from the `Article` model. This event was responsible for automatically generating the `url_hash` on save.
    
    This logic is now handled manually at the component level, making the model event redundant.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -15,18 +15,6 @@ class Article extends Model
         'published_at' => 'date',
     ];
 
-    public static function boot()
-    {
-        parent::boot();
-
-        // Otomatis isi url_hash saat membuat atau mengupdate artikel
-        static::saving(function ($article) {
-            if ($article->isDirty('url')) {
-                $article->url_hash = md5($article->url);
-            }
-        });
-    }
-
     public function publisher()
     {
         return $this->belongsTo(Publisher::class);
